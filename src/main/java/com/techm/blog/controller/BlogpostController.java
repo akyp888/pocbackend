a7a84c815e1e0be24a123b5927de6652a1f9ac4c
akyp888 <akashrkashyap98@gmail.com>
2021-08-16 13:01:15 +0530

    ip

--- src/main/java/com/techm/blog/controller/BlogpostController.java
+++ src/main/java/com/techm/blog/controller/BlogpostController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "http://52.206.79.151:5000/")
 @RestController
 @RequestMapping("api/")
 @AllArgsConstructor
